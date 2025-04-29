ffe4aaa06320da7a65703edb789fe28bd1db09f9
EBI S PAUL <132058452+ebispaul07@users.noreply.github.com>
2025-04-29 11:26:40 +0530

    Update problem1.cpp

--- assessments/week02/src/problem1.cpp
+++ assessments/week02/src/problem1.cpp
@@ -1,41 +1,43 @@
 //Number Star Pattern
 
+
 #include<iostream>
 using namespace std;
 
-int main() {
-	int num;
-	cout << "Enter Any Number:";
-	cin >> num;
-
-	int count = 0;
-
-	for (int i = 1;i <= num;i++) {
-		for (int j = 1;j <= i;j++) {
-
-			cout << "*";
-			count++;
-			cout << count;
-
-
-		}
-
-		cout << endl;
-
-	}
-
-
-
-		for (int i = num - 1;i >= 1;i--) {
-			for (int j = 1;j <= i;j++) {
-				cout << "*";
-
-				count--;
-				cout << count;
-			}
-			cout << endl;
-		}
-
-
-	
+int main(){
+    int num;
+    cout<<"Enter Any Number:";
+    cin>>num;
+    
+    cout<<"*"<<endl;
+    
+    for(int i=1;i<=num;i++){
+        cout<<"*";
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        
+        for(int j=i-1;j>=1;j--){
+            cout<<j;
+        }
+        cout<<"*";
+        cout<<endl;
+    }
+    
+    for(int i=num-1;i>=1;i--){
+        cout<<"*";
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        
+        for(int j=i-1;j>=1;j--){
+            cout<<j;
+        }
+        cout<<"*";
+        cout<<endl;
+    }
+    
+    cout<<"*";
+    
+    
 }
